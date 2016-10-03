c50efdc5e833f643a27b053ed843e5055fc77503
sofo01 <sofoklis_the@hotmail.com>
2016-10-03 18:46:41 +0100

    Create methods shout, repeat, start_of_word

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,3 +1,17 @@
-def echo
-  "hello"
+def echo(something)
+  say_something
+end
+
+def shout(something)
+  say_something.upcase
+end
+
+def repeat(something, times = 2)
+  word = something + " "
+  result = (word * times).strip
+  return result
+end
+
+def start_of_word (word, length)
+  return word[0..length-1]
 end
