a2331a5ae3098857e0ab22cad8a174b589e58ee8
sofo01 <sofoklis_the@hotmail.com>
2016-10-03 19:01:04 +0100

    Create methods first_word & Titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -15,3 +15,29 @@ end
 def start_of_word (word, length)
   return word[0..length-1]
 end
+
+def first_word (words)
+  array_of_words = words.split(" ")
+  return array_of_words[0]
+end
+
+def titleize(word)
+  array = word.split(" ")
+  little_words = ['the','and','is','a','an','over','in','on','of','or'] #Am I missing any ?
+  result = ""
+
+  array.each do |word|
+    if ((little_words).include?(word))
+      word.downcase
+    else
+      word.capitalize!
+    end
+  end
+  array[0].capitalize!
+
+  array.each do |word|
+    result += word + " "
+  end
+
+  return result.strip
+end
